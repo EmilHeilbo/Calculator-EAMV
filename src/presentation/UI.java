914b6d31d318a488947b90dccaad9d5d23b7f97c
Simeon J. Frederiksen <33501698+QSyNeXT@users.noreply.github.com>
2020-10-06 13:56:02 +0200

    Added a simple JavaFx Stage and scene

--- src/presentation/UI.java
+++ src/presentation/UI.java
@@ -1,10 +1,24 @@
 package presentation;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import logic.*;
 
-public class UI {
+public class UI extends Application{
     public static void main(String[] args) {
-        var calc = new Calculator();
-        System.out.println(calc.add(1, 6));
+    	launch(args);
     }
+
+	@Override
+	public void start(Stage pStage) throws Exception {
+		pStage.setTitle("Calculator title");
+		GridPane g = new GridPane();
+		g.add(new Text("Hello"), 0, 0);
+		Scene s = new Scene(g);
+		pStage.setScene(s);
+		pStage.show();
+	}
 }
