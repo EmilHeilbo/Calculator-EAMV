7e2cfad8fd595fcc2028a6fb7a744b862437976a
mlad0064 <72009488+mlad0064@users.noreply.github.com>
2020-10-09 11:12:36 +0200

    30 min

--- src/presentation/UI.java
+++ src/presentation/UI.java
@@ -3,7 +3,9 @@ package presentation;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import logic.*;
 
@@ -17,10 +19,18 @@ public class UI extends Application{
 	@Override
 	public void start(Stage pStage) throws Exception {
 		final int BUTTON_SIZE = 80;
-
 		pStage.setTitle("EAMV - Simple Calculator");
+		pStage.setWidth(4 * BUTTON_SIZE);
+		pStage.setHeight(7 * BUTTON_SIZE);
+
+		VBox vbox = new VBox();
 		GridPane g = new GridPane();
-		Scene s = new Scene(g, BUTTON_SIZE * 4, BUTTON_SIZE * 7);
+		Text text = new Text("Smashkey");
+		text.setTextAlignment(TextAlignment.RIGHT);
+		vbox.getChildren().add(text);
+		
+		Scene s = new Scene(vbox);
+		
 		pStage.setScene(s);
 		pStage.show();
 	}
