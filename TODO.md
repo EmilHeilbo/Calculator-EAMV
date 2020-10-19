3af6f031542804632b3514e7153b1973a4d5949a
Simeon J. Frederiksen <33501698+QSyNeXT@users.noreply.github.com>
2020-10-19 16:42:45 +0200

    Not XP with Emil, but fun and hot drinks :) Look at todo.md

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO List
+
+- [ ] Lav en Enum __RegneOperation__ med de forskellige regneoperationer som bruger to argumenter
+- [ ] Lav en instans variable af vores Enum i vores UI, til at huske hvilken regneoperation vi vil bruge  
+- [ ] Lav en class __Beregning__ som extender EventHandler<ActionListener>, og har en constructor med parameter __RegneOperation__, som gemmens i en instans variable, og bruges i dens override'ed metode, til at gemme regneoperationen i vores UI.
+- [ ] Tal, Enter og regneoperationer med et argument fungere anderledes, og skal have deres egen EventHandlers. Lave nye classer til dem, eller bruge inner classes?
--- src/presentation/UI.java
+++ src/presentation/UI.java
@@ -1,9 +1,15 @@
 package presentation;
 
+import javafx.scene.control.TextField;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.*;
@@ -12,13 +18,14 @@ import javafx.stage.Stage;
 import logic.*;
 
 public class UI extends Application {
+	private Calculator calc;
     public static void main(String[] args) {
-        var calc = new Calculator();
         launch(args);
     }
 
     @Override
     public void start(Stage pStage) {
+    	calc = new Calculator();
         final int BUTTON_SIZE = 80;
         final int SPACING = 10;
         final int WIDTH = 4;
@@ -27,13 +34,15 @@ public class UI extends Application {
         pStage.setTitle("EAMV - Simple Calculator");
         pStage.setWidth(BUTTON_SIZE * WIDTH + SPACING * (WIDTH + 1));
         pStage.setHeight(BUTTON_SIZE * HEIGHT + SPACING * (HEIGHT + 1));
+        
         var grid = new GridPane();
-        grid.setMinSize(WIDTH * BUTTON_SIZE + SPACING * (WIDTH + 1), HEIGHT * BUTTON_SIZE + SPACING * (HEIGHT + 1));
+        //grid.setPrefSize(BUTTON_SIZE, BUTTON_SIZE);
+        //grid.setMinSize(WIDTH * BUTTON_SIZE + SPACING * (WIDTH + 1), HEIGHT * BUTTON_SIZE + SPACING * (HEIGHT + 1));
         grid.setHgap(SPACING);
         grid.setVgap(SPACING);
         String[] buttonText =
                 {
-                        "%", "||", "^", "√",
+                        "%", "|x|", "^", "Sqrt",
                         "+", "-", "*", "/",
                         "7", "8", "9", "cos",
                         "4", "5", "6", "sin",
@@ -42,8 +51,10 @@ public class UI extends Application {
                 };
         Button[] buttons = new Button[24];
         var scene = new Scene(grid);
-        var equationDisplay = new Text("Test");
-        equationDisplay.setTextAlignment(TextAlignment.RIGHT);
+        var equationDisplay = new Label("0");
+        equationDisplay.setAlignment(Pos.CENTER_RIGHT);
+        equationDisplay.setPrefHeight(BUTTON_SIZE);
+        equationDisplay.setStyle("-fx-font: 40 wingdings;");
         GridPane.setColumnSpan(equationDisplay, 4);
         GridPane.setFillWidth(equationDisplay, true);
         GridPane.setConstraints(equationDisplay, 0, 0);
@@ -51,10 +62,13 @@ public class UI extends Application {
         for (int i = 0; i < buttons.length; i++) {
             buttons[i] = new Button("Button-" + (i + 1));
             buttons[i].setText(buttonText[i]);
+            buttons[i].setPrefSize(BUTTON_SIZE, BUTTON_SIZE);
             GridPane.setConstraints(buttons[i], i % WIDTH, i / WIDTH + 1);
             grid.getChildren().add(buttons[i]);
         }
 
+        
+        
         grid.setBackground(new Background(new BackgroundFill(Color.web("B8AFA9"), CornerRadii.EMPTY, Insets.EMPTY)));
         pStage.setScene(scene);
         pStage.show();
